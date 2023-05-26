2db6906100d2d99850ace64a108f5a4f649485ae
21373335 <21373335@buaa.edu.cn>
2023-05-26 15:06:57 +0800

    recycleBin api

--- src/main/java/top/leonardsaikou/backend/mapper/FlightMapper.java
+++ src/main/java/top/leonardsaikou/backend/mapper/FlightMapper.java
@@ -3,6 +3,7 @@ package top.leonardsaikou.backend.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import top.leonardsaikou.backend.entity.Flight;
 
 import java.util.List;
@@ -12,6 +13,16 @@ public interface FlightMapper extends BaseMapper<Flight> {
 
     @Select("select * from flights where departure_city = #{departureCity} or arrival_city = #{arrivalCity}")
     List<Flight> selectByCity(String departureCity, String arrivalCity);
+
+    @Select("select * from flights where status = '回收站中'")
+    List<Flight> selectRecycleBin();
+
+    @Update("update flights set status = '未发布' where id = #{id} and status = '回收站中'")
+    int recoverFlight(String id);
+
+    //查询：前端提供航班号，查找对应航班号且状态为“回收站中”的航班。
+    @Select("select * from flights where id = #{id} and status = '回收站中'")
+    List<Flight> selectRecycleBinById(String id);
 
 
 }
