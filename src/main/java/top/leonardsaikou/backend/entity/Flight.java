7119d3348c1bd2936fc1edd9358242dd5724dd93
21373335 <21373335@buaa.edu.cn>
2023-05-26 16:57:38 +0800

    debug

--- src/main/java/top/leonardsaikou/backend/entity/Flight.java
+++ src/main/java/top/leonardsaikou/backend/entity/Flight.java
@@ -5,7 +5,7 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 @Entity
-@Table(schema = "se_db",name = "flights")
+@Table(schema = "se_db", name = "flights")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
