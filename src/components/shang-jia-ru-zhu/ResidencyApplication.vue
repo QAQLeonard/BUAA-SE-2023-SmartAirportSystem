932d21289e416014502e6d55330c077021d70134
jusasusual <88149103+jusasusual@users.noreply.github.com>
2023-05-26 17:36:17 +0800

    Add files via upload

--- src/components/shang-jia-ru-zhu/ResidencyApplication.vue
+++ src/components/shang-jia-ru-zhu/ResidencyApplication.vue
@@ -1,84 +1,101 @@
 
 <template class="rsd">
-    <el-form class="elf" label-position="left" label-width="150px" style="max-width: 460px">
-      <el-space fill>
-        <el-alert type="info" show-icon :closable="false">
-        <p>请输入您要申请的店铺地址</p>
-        </el-alert>
-        <el-form-item label="Shop Location">
-            <i class="fa fa-id-card"></i>
-            <el-input v-model="ShopForm.ShopName" />
-        </el-form-item>
-      </el-space>
-      <el-divider border-style="dotted" />
-      <el-space fill>
-        <el-alert type="info" show-icon :closable="false">
-          <p>
-            请输入店主的姓名和手机号
-          </p>
-        </el-alert>
-        <el-form-item label="Your Information">
-        <i class="fa fa-user-o"></i>
-              <el-input
-                v-model="ShopForm.PersonName"
-                label="Your Name"
-                placeholder="Your Name"
-              />
-              <el-divider border-style="dashed" />
-              <el-input
-                v-model="ShopForm.PhoneNumber"
-                label="Contact"
-                placeholder="Contact"
-              />
-        </el-form-item>
-      </el-space>
-      <el-divider border-style="dotted" />
-      <template>
-        <el-popconfirm
-          confirm-button-text="Yes"
-          cancel-button-text="No"
-          title="Are you sure to submit this?"
-          @confirm="confirmEvent"
-          @cancel="cancelEvent"
-        >
-          <template #reference>
-            <el-button>Submit</el-button>
-          </template>
-        </el-popconfirm>
-      </template>
-    </el-form>
-  </template>
-  
-  <script lang="ts" setup>
-  import { reactive } from 'vue'
-  //import { InfoFilled } from '@element-plus/icons-vue'
+  <el-form class="elf" label-position="left" label-width="150px" style="max-width: 460px">
+    <el-space fill>
+      <el-alert type="info" show-icon :closable="false">
+      <p>请输入您要申请的店铺地址</p>
+      </el-alert>
+      <el-form-item label="Shop Information">
+          <i class="fa fa-id-card"></i>
+          <el-input
+              v-model="ShopForm.ShopName"
+              label="Shop Name"
+              placeholder="Shop Name"
+            />
+            <el-divider border-style="dashed" />
+            <el-input
+              v-model="ShopForm.ShopLocation"
+              label="Shop Location"
+              placeholder="Shop Location"
+            />
+      </el-form-item>
+    </el-space>
+    <el-divider border-style="dotted" />
+    <el-space fill>
+      <el-alert type="info" show-icon :closable="false">
+        <p>
+          请输入店主的姓名和手机号
+        </p>
+      </el-alert>
+      <el-form-item label="Your Information">
+      <i class="fa fa-user-o"></i>
+            <el-input
+              v-model="ShopForm.PersonName"
+              label="Real Name"
+              placeholder="Real Name"
+            />
+            <el-divider border-style="dashed" />
+            <el-input
+              v-model="ShopForm.UserName"
+              label="User Name"
+              placeholder="User Name"
+            />
+            <el-divider border-style="dashed" />
+            <el-input
+              v-model="ShopForm.PhoneNumber"
+              label="Contact"
+              placeholder="Contact"
+            />
+      </el-form-item>
+    </el-space>
+    <el-divider border-style="dotted" />
+    <template>
+      <el-popconfirm
+        confirm-button-text="Yes"
+        cancel-button-text="No"
+        title="Are you sure to submit this?"
+        @confirm="confirmEvent"
+        @cancel="cancelEvent"
+      >
+        <template #reference>
+          <el-button>Submit</el-button>
+        </template>
+      </el-popconfirm>
+    </template>
+  </el-form>
+</template>
 
-  const confirmEvent = () => {
-    console.log('confirm!')
-  }
-  const cancelEvent = () => {
-    console.log('cancel!')
-  } 
-  const ShopForm = reactive({
-    ShopName: '',
-    PersonName: '',
-    PhoneNumber: '',
-  })
-  </script>
-  <style lang="scss" scoped>
-  .rsd{
+<script lang="ts" setup>
+import { reactive } from 'vue'
+//import { InfoFilled } from '@element-plus/icons-vue'
+
+const confirmEvent = () => {
+  console.log('confirm!')
+}
+const cancelEvent = () => {
+  console.log('cancel!')
+} 
+const ShopForm = reactive({
+  ShopName: '',
+  PersonName: '',
+  PhoneNumber: '',
+  ShopLocation:'',
+  UserName:'',
+})
+</script>
+<style lang="scss" scoped>
+.rsd{
+  width: 100%;
+  height: 100%;
+  position: relative;
+  font-style: oblique;
+  .elf{
     width: 100%;
     height: 100%;
     position: relative;
     font-style: oblique;
-    .elf{
-      width: 100%;
-      height: 100%;
-      position: relative;
-      font-style: oblique;
-      background-color: aquamarine;
-    }
+    background-color: aquamarine;
   }
-  </style>
+}
+</style>
 
-  
